51265a4b636f44bb689eb3041c74cd403159c561
Joaquin Rojkind <joaquin.rojkind@Joaquins-MacBook-Pro.local>
2022-05-10 10:48:50 -0300

    Update README, refactor creation/update dtos

--- src/main/java/com/metadata/registration/api/dto/StudentCreationUpdateDto.java
+++ src/main/java/com/metadata/registration/api/dto/StudentCreationUpdateDto.java
@@ -1,12 +1,12 @@
 package com.metadata.registration.api.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentCreationUpdateDto {
 
     private String name;
--- src/main/java/com/metadata/registration/api/exception/RegistrationExceptionHandler.java
+++ src/main/java/com/metadata/registration/api/exception/RegistrationExceptionHandler.java
@@ -2,21 +2,21 @@ package com.metadata.registration.api.exception;
 
 import com.metadata.registration.service.exception.RegistrationValidationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class RegistrationExceptionHandler {
 
     @ExceptionHandler(RegistrationValidationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorBody registrationValidationException(RegistrationValidationException ex, WebRequest request) {
+    public ResponseEntity<ErrorBody> registrationValidationException(RegistrationValidationException ex, WebRequest request) {
         ErrorBody errorBody = ErrorBody.builder()
                 .errorCode(ex.getErrorCode().name())
                 .message(ex.getMessage())
                 .build();
-        return errorBody;
+        return new ResponseEntity<>(errorBody, HttpStatus.BAD_REQUEST);
     }
 }
